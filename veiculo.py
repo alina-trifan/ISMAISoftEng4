21b03bc11b6b546646cef1258effc4d71df80e59
Bruno Matos <brunodm100@gmail.com>
2020-03-07 01:27:50 +0000

    Ficheiro da classe veiculo adicionado

---
+++ veiculo.py
@@ -0,0 +1,26 @@
+class Veiculo:
+    matricula = ""#redundante
+    marca = ""#redundante
+
+    def __init__(self, l_matricula, l_marca="indefinido"):
+        self.__matricula = l_matricula
+        self.__marca = l_marca
+
+    def getMatricula(self):
+        return self.__matricula
+
+    def __repr__(self):
+        return {'Matricula':self.__matricula, 'Marca':self.__marca}
+
+    def __str__(self):
+        return 'Matricula = '+self.__matricula+', Marca = '+str(self.__marca)
+
+    def copy(self):
+        newcopy = self
+        return newcopy
+        __copy__ = copy
+
+    def __eq__(self, other):
+        if isinstance(other, Veiculo):
+            return self.__matricula == other.__matricula
+        return False
