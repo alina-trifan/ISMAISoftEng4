529438de7c70b8bc3c6e9c4355add6098547665c
Daniel Moreira <61556403+a034734@users.noreply.github.com>
2020-03-10 11:30:35 +0000

    Update b.py

--- b.py
+++ b.py
@@ -2,6 +2,14 @@ import csv
 import operator
 
 def printClients(v):
-    sort = sorted(vehicles,key=operator.itemgetter(2))
-    for linha in sort:
-        print(f'\t{linha[2]} : ({linha[0]}, {linha[1]})')
+    clientes=[]
+    if not vehicles:
+        print("\nNão existem registos.")
+    for i in range(0,len(vehicles)):
+        clientes.append(vehicles[i][2])
+    clientes.sort()
+    clientes = list(dict.fromkeys(clientes))
+    for i in range(0,len(clientes)):
+        for a in range(0,len(vehicles)):
+            if (clientes[i]==vehicles[a][2]):
+                print("NIF:",clientes[i],"Matrícula:",vehicles[a][0],"Marca:",vehicles[a][1])
