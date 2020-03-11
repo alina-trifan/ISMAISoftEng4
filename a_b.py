9bea38a70bf694ae5d1fcdb56f38a0e3954025f5
marcostrike11 <61556449+marcostrike11@users.noreply.github.com>
2020-03-11 15:53:08 +0000

    Exercicio A, B e D

---
+++ a_b.py
@@ -0,0 +1,24 @@
+#Mostrar dados do ficheiro e contar linhas
+import csv
+import operator
+vehicle=[]
+contador=0
+Lista = open('ep1.csv')
+Ficheiro = csv.reader(Lista, delimiter=';')
+for linha in Ficheiro:
+    print(linha[2], ": (", linha[0], ",", linha[1], ")")
+    vehicle.append(linha)
+    contador +=1
+print("Contador: ",contador)
+
+
+
+
+#ordenar lista importada
+
+#ordenar = sorted(vehicle,key=lambda linha: linha[2])
+ordenar = sorted(vehicle,key=operator.itemgetter(2))
+for linha in ordenar:
+    print(linha[2], ": (", linha[0], ",", linha[1], ")")
+
+
---
+++ e.py
@@ -0,0 +1,31 @@
+import re
+def inicio():
+    duracao = ""
+    i = 0 #contador para a duração
+    p = 0 # contador para a matricula
+    matricula = ""
+    x = ""
+    
+    while p != 1:
+        matricula = input("Introduza a matricula no formato 00-AA-00\n")
+        if verifica_matricula(matricula) == None:
+            print("Não introduziu uma matricula válida")
+        else:
+            print (matricula)
+            p +=1
+            
+    while i != 1:        
+        duracao = input("Introduza a duração\n")
+        if duracao.isdigit() and int(duracao) > 0:
+            print(duracao)
+            i += 1
+        else:
+            print("Não introduziu um numero válido")        
+
+def verifica_matricula (string):  
+    match = re.search(r'\d{2}-[a-zA-Z]{2}-\d{2}', string)
+    return None if match == None\
+        else ()
+# inicio do programa
+inicio()
+
