ec241c81912739a64b9f418265ad027a6d6f581c
Marcoafnunes <61556551+Marcoafnunes@users.noreply.github.com>
2020-03-11 11:58:11 +0000

    Delete metodoB.py

--- metodoB.py
+++
@@ -1,8 +0,0 @@
-import csv
-import operator
-
-def printCliente():
-    lista = vehicles
-    lista.sort(key=operator.itemgetter(2))
-    for row in lista:
-        print(f'{row[2]} : ({row[0]}, {row[1]})')
