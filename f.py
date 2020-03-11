ea1c95e751de7f79b3ff8026a308d288f26e42ec
sararocha25 <sararaquel2621@gmail.com>
2020-03-11 23:38:49 +0000

    Update f.py
    Adicionar var

--- f.py
+++ f.py
@@ -1,9 +1,9 @@
 import csv
 
 def saveEntries(l):
-    cont = len()
+    count = len(operations)
     with open('parque.csv',newline='',delimiter=";") as file:
         writer_csv = csv.writer(file)
-        for i in range(0, cont):
-            writer_csv.writerow()
+        for i in range(0, count):
+            writer_csv.writerow(operations[i])
     return writer_csv
