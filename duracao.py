f2ffcb59a9dcd1ef8b3788c0c74e8f30a42bda04
Layla Andrade <61556521+reislayla@users.noreply.github.com>
2020-03-11 23:58:04 +0000

    Atualização e correção dos metodos D & F

--- duracao.py
+++ duracao.py
@@ -3,6 +3,7 @@ class Duracao:
     def validDuration():
 
         exitFlag = 0
+        durationFlag = False
 
         while True:
             d = input("Digite a duração (0 ou Enter para sair): ")
@@ -14,6 +15,7 @@ class Duracao:
                 elif int_d > 0:
                     d = int_d
                     exitFlag = 0
+                    durationFlag = True
                     break
                 else:
                     str_d = str(int_d)
@@ -22,10 +24,10 @@ class Duracao:
                         break
                     else:                     
                         print("Valor inválido")
+                        durationFlag = False
                 break            
             else:
                 exitFlag = 1 
                 break
             break	
-        return d, True, exitFlag
-
+        return d, durationFlag, exitFlag
