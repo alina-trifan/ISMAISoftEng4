8493f4e9237db33701a513e1306cd3456d4e713e
marcostrike11 <61556449+marcostrike11@users.noreply.github.com>
2020-03-11 21:47:43 +0000

    exercicios a, b, e

--- parking.py
+++ parking.py
@@ -18,23 +18,53 @@ def menu(text):
 
 
 def loadClients ():
-    ...
+    contador=0
+    l = []
+    Lista = open('ep1.csv')
+    Ficheiro = csv.reader(Lista, delimiter=';')
+    for linha in Ficheiro:
+        l.append(linha) 
+        contador +=1
+    return l
 
 
 def printClients(v):
-    ...
+    print(l)
+    ordenado = []
+    ordenar = sorted(l,key=operator.itemgetter(2))
+    #return ordenar
+    for i in range(len(ordenar)):
+        #print(v)
+        print(l[2], ": (", l[0], ",", l[1], ")") 
         
 def saveEntries(l):
     ...
         
 def addParkEntry():
-   ...
+   duracao = ""
+   matricula = ""
+   i = 0 #contador para a duração
+   p = 0 # contador para a matricula
+   while p != 1:
+       matricula = input("Introduza a matricula no formato 00-AA-00\n")
+       if verifica_matricula(matricula) == False:
+           print("Não introduziu uma matricula válida")
+       else:
+           p +=1 # validou a matricula e passa para o passo seguinte
+   return matricula
+   while i != 1:        
+       duracao = input("Introduza a duração\n")
+       if duracao.isdigit() and int(duracao) > 0:
+           i += 1 #validou a duração e passa para o passo seguinte
+       else:
+           print("Não introduziu um numero válido")
+   print(matricula," - ",duracao)
+   return duracao
     
-
 def validPlate(s):
 	match = re.search(r"[0-9]{2}[-][a-zA-Z]{2}[-][0-9]{2}", string)  
     	return False if match == None\
-        	else ()
+        	else True
 
 	
 def matches(s, pattern):
