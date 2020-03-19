2c27a59c6f3d21591b43b896b6dfcad71af7b464
AndreM-coder <andre.moura108@hotmail.com>
2020-03-19 19:30:15 +0000

    if removal

--- parking.py
+++ parking.py
@@ -43,13 +43,10 @@ def addParkEntry():
 # d.	Escreva uma função que valide se uma string, passada como argumento, representa uma matrícula válida em Portugal. Considere apenas matrículas 
 # posteriores a 2005 compostas por letras no meio como no seguinte exemplo: 00-AA-00. A função deverá devolver um valor lógico Verdadeiro se a matrícula for válida e Falso, caso contrário.
 def validPlate(matricula):
-    if matricula.count("-") == 2 and len(matricula) == 8:
         if re.findall("^(\d{2}-[A-Z]{2}-\d{2})$", matricula):
            return True
         else:
             return False
-    else:
-        return False
 
 def matches(s, pattern):
     ...
